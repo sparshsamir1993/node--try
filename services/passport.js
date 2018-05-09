a7a7b251c7ebe221ca5dda2e5df33f3a7a1a3b71
Sparsh <sparshsamir1993@gmail.com>
2018-05-09 17:45:59 +0530

    oauth

---
+++ services/passport.js
@@ -0,0 +1,62 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys.js");
+const user = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  user.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      user.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new user({ googleId: profile.id, name: profile.displayName })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
+
+// passport.use(
+//   new FacebookStrategy({
+//     clientID: keys.facebookAppID,
+//     clientSecret: keys.facebookAppSecret,
+//     callbackURL: "/auth/facebook/callback"
+//   },
+//     (accessToken, refreshToken, profile, cb) => {
+//       user.findOne({facebookId: profile.id}).then( existingFBUser =>{
+//         if(existingFBUser){
+
+//         }
+//         else{
+//           user.findOne({email: profile.email}).then( existingUser =>{
+//             if(existingUser){
+
+//             }
+//             else{
+//               new user({facebookId: profile.id})
+//             }
+//           })
+//         }
+//       })
+//     })
+// );
