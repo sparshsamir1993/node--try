d4a7a97a7d45f9d1ede73922dbfab0903c4fe9bb
Sparsh <sparshsamir1993@gmail.com>
2018-05-12 12:02:31 +0530

    oauth done and dep properties

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+    googleClientID : process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret : process.env.GOOGLE_CLIENt_SECRET,
+    facebookAppID :"1023999171058440",
+    facebookAppSecret : "0053a208741c31eeed5d8d03c156540d",
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+
+}
+
