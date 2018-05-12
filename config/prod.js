356f0874190808104eef562def35e0e3b0089104
Sparsh <sparshsamir1993@gmail.com>
2018-05-12 12:16:50 +0530

    prof keys typo fixed

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     googleClientID : process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret : process.env.GOOGLE_CLIENt_SECRET,
+    googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
     facebookAppID :"1023999171058440",
     facebookAppSecret : "0053a208741c31eeed5d8d03c156540d",
     mongoURI: process.env.MONGO_URI,
